23e529827dff61d6920c03dc06e0edfc18d1ff3b
Arthur Pavezzi <arthur.henrique.pavezzi@gmail.com>
2025-04-27 21:07:13 -0300

    Modificando banco de dados, adicionando DDL separado

--- ddl.sql
+++ ddl.sql
@@ -3,20 +3,24 @@ CREATE TABLE IF NOT EXISTS calendar (
     dia_do_ano INTEGER NOT NULL,
     ano INTEGER NOT NULL,
     data DATE NOT NULL,
+    mes INTEGER NOT NULL,
+    dia INTEGER NOT NULL,
     dia_da_semana INTEGER NOT NULL,
     nivel_jejum INTEGER NOT NULL
 );
+
 CREATE TABLE IF NOT EXISTS feasts (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    id_calendar INTEGER NOT NULL,
+    mes INTEGER NOT NULL,
+    dia INTEGER NOT NULL,
+    jurisdicao CHAR(1) NOT NULL, -- 'P' = Polonesa, 'G' = Grega
     categoria INTEGER NOT NULL,
-    matinas TEXT,
-    vesperas TEXT,
-    vigilia TEXT,
-    epistola TEXT,
-    evangelho TEXT,
-    troparion TEXT,
-    scripture_ref TEXT,
-    scripture_text TEXT,
-    FOREIGN KEY (id_calendar) REFERENCES calendar(id)
+    matinas TEXT NOT NULL DEFAULT '',
+    vesperas TEXT NOT NULL DEFAULT '',
+    vigilia TEXT NOT NULL DEFAULT '',
+    epistola TEXT NOT NULL DEFAULT '',
+    evangelho TEXT NOT NULL DEFAULT '',
+    troparion TEXT NOT NULL DEFAULT '',
+    scripture_ref TEXT NOT NULL DEFAULT '',
+    scripture_text TEXT NOT NULL DEFAULT ''
 );
